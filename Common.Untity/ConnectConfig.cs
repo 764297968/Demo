7bd8653343114f62d64c7ab87a1e2bde3fdc1472
764297968@qq.com <764297968@qq.com>
2017-05-05 23:38:34 +0800

    要

---
+++ Common.Untity/ConnectConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Untity
+{
+    public static class ConnectConfig
+    {
+        public static string ConnString()
+        {
+             return System.Configuration.ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+        }
+        public static string ConnString(string connectAddress, string dataBase, string userName, string passWord)
+        {
+            string str = string.Format(System.Configuration.ConfigurationManager.ConnectionStrings["ConnString"].ToString(), connectAddress, dataBase, userName, passWord);
+            return str;
+
+        }
+    }
+}
---
+++ WebApp/Controllers/ConnectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ConnectController : Controller
+    {
+        // GET: Connect
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddConnect()
+        {
+            return View();
+        }
+    }
+}
